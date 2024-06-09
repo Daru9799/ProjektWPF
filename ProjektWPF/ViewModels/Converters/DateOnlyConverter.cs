27be1013e231e485473bff71db04f7d39ff3ecb0
JakubKempny <jakub.kempny01@onet.eu>
2024-06-09 17:36:26 +0200

    Zmiany w WorkoutPlans
    Teraz wyświetla tylko datę w kolumnie Data utworzenia

---
+++ ProjektWPF/ViewModels/Converters/DateOnlyConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjektWPF.ViewModels.Converters
+{
+    public class DateOnlyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("d", CultureInfo.CurrentCulture); // "d" short date pattern
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
